c5c4fa07a5df42fef6a7af39486660e720497404
luoxue <253143003@qq.com>
2019-11-27 10:40:10 +0800

    第二次提交

--- HelloWorld/src/demo/HelloWorld.java
+++ HelloWorld/src/demo/HelloWorld.java
@@ -3,6 +3,7 @@ package demo;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		System.out.println("");
+		System.out.println("hh");
+		System.out.println("123");
 	}
 }
