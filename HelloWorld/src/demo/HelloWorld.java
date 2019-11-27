cf0bc07075e338bcc39b7a7c997894c31b2799ae
luoxue <253143003@qq.com>
2019-11-27 10:45:02 +0800

    第三次提交

--- HelloWorld/src/demo/HelloWorld.java
+++ HelloWorld/src/demo/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hh");
-		System.out.println("123");
+		System.out.println("1");
 	}
 }
